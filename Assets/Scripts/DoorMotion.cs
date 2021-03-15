86698ee4a6766208e90c56f1ed2f066c54cf03fd
VictorTaubkin <68503294+VictorTaubkin@users.noreply.github.com>
2021-03-16 00:04:12 +0200

    Initial commit

---
+++ Assets/Scripts/DoorMotion.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class DoorMotion : MonoBehaviour
+{
+    private Animator anim;
+    // Start is called before the first frame update
+    void Start()
+    {
+        anim = GetComponent<Animator>();
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        anim.SetBool("opening", true);
+    }
+
+    private void OnTriggerExit(Collider other)
+    {
+        anim.SetBool("opening", false);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/Motion.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Motion : MonoBehaviour
+{
+    private CharacterController controller;
+    private float speed = 0.1f;
+    private float angularSpeed = 1f;
+    private float rx=0f,ry;
+    public GameObject playerCamera; // public allows to init this object FROM UNITY!!!
+    // Start is called before the first frame update
+    void Start()
+    {
+        controller = GetComponent<CharacterController>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float dx, dz;
+        // simple motion
+        // transform.Translate(new Vector3(0, 0, 0.1f));
+
+        // mouse input
+        rx -= Input.GetAxis("Mouse Y") * angularSpeed; // vertical rotation
+        playerCamera.transform.localEulerAngles = new Vector3(rx, 0, 0);// runs on playerCamera
+        // ry is horizontal rotation
+        ry = transform.localEulerAngles.y+ Input.GetAxis("Mouse X") * angularSpeed;
+        
+        transform.localEulerAngles = new Vector3(0, ry, 0); // runs on this (Player)
+        // keyboard input
+        dx = Input.GetAxis("Horizontal")*speed;
+        dz = Input.GetAxis("Vertical")*speed;
+        Vector3 motion = new Vector3(dx, 0, dz);
+        motion = transform.TransformDirection(motion); // now in Global coordinates
+        controller.Move(motion);
+    }
+}
